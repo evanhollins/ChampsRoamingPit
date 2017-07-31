4d163142eadd077a7cc087959f91c55ba6a61a4d
evanhollins <n1nj@J3su$!>
2017-07-31 15:52:22 +1000

    Added GUI using tk

--- Server.py
+++ Server.py
@@ -1,19 +1,92 @@
 import waitress
 from Application import app
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+import threading
 
-start = Tk()
-Label(start, text="Which port should the server run on?").grid(row = 0, column = 0)
-e1 = Entry(start)
-e1.grid(row = 1, column = 0)
-e1.insert(0, "8080")
-Button(start, text='Enter', command=start.quit()).grid(row = 2, column=0)
-start.mainloop()
+serverPort = 8080
 
-# top = Tk()
-# w = Label(top, text="Champs Inspection Software", font="Arial 18 bold")
-# w.pack()
-#
-# top.mainloop()
 
-# waitress.serve(app, listen="*:8080")
+class WebServerThread(threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+    def run(self):
+        waitress.serve(app, listen="*:%i" % serverPort)
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Choose Server Port", font="Arial 12")
+        label.pack(pady=10, padx=10)
+
+        e1 = tk.Entry(self, text = "Port:")
+        e1.insert(0, "8080")
+        e1.pack(pady=10, padx=10)
+
+        button = tk.Button(self, text = "Start Server", command=lambda: self.transition(e1.get(), controller))
+        button.pack(pady=10, padx=10)
+
+    def transition(self, port, controller):
+        try:
+            global serverPort
+            serverPort = int(port)
+            controller.show_frame(Master)
+        except:
+            pass
+
+    def start(self):
+        pass
+
+
+class Master(tk.Frame):
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        title = tk.Label(self, text = "TBA Analyze Server", font = "Arial 18 bold")
+        title.pack()
+
+        quitButton = tk.Button(self, text = "Quit", command=lambda: self.prompt())
+        quitButton.pack()
+
+    def prompt(self):
+        if messagebox.askyesno("Quit", "Are you sure you want to quit?"):
+            serverThread.stop()
+            self.quit()
+
+
+    def start(self):
+        serverThread.start()
+
+
+class ServerGUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, Master):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+        frame.start()
+
+serverThread = WebServerThread(1, "serverThread")
+localApp = ServerGUI()
+localApp.mainloop()
